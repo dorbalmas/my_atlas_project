45908c6cef0505bb39f1f1781000e537ee0303ca
dorbalmas <69203785+dorbalmas@users.noreply.github.com>
2020-09-07 18:21:24 +0300

    Update ItemComp.js

--- src/movie_project_comp/ItemComp.js
+++ src/movie_project_comp/ItemComp.js
@@ -1,41 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { getApi } from "../services/apiService";
 
 import Swal from "sweetalert2";
 function ItemComp(props) {
   let item = props.item;
-  let [arr, setArr] = useState([]);
-  let [url, setUrl] = useState(null);
-  useEffect(() => {
-    url = `https://www.omdbapi.com/?i=${item.imdbID}&apikey=e04a6b04`;
-    getApi(url).then((data) => {
-      setArr(data);
-    });
-  }, [url]);
+  let movieArr = props.movieArr;
+  let i = 1;
 
-  const popup = () => {
-    setUrl(`https://www.omdbapi.com/?i=${item.imdbID}&apikey=e04a6b04`);
-    Swal.fire({
-      title: arr.Title + " " + arr.Year,
-      text: arr.Plot,
-      imageUrl: arr.Poster,
-      imageWidth: 300,
-      imageHeight: 300,
-      imageAlt: "Custom image",
-      confirmButtonText: "More info",
-    }).then((result) => {
-      if (result.value) {
-        url = `https://www.omdbapi.com/?i=${item.imdbID}&apikey=e04a6b04&plot=full`;
-        getApi(url).then((data2) => {
-          console.log(data2);
-          Swal.fire({ text: data2.Plot });
+  const popup = (_item) => {
+    console.log(_item);
+    getApi(`https://www.omdbapi.com/?i=${_item}&apikey=e04a6b04`).then(
+      (data1) => {
+        Swal.fire({
+          title: data1.Title + " " + data1.Year,
+          text: data1.Plot,
+          imageUrl: data1.Poster,
+          imageWidth: 300,
+          imageHeight: 300,
+          imageAlt: "Custom image",
+          showCloseButton: true,
+          showCloseButton: true,
+          confirmButtonText: "next movie",
+        }).then((result) => {
+          if (result.value) {
+            let movie = movieArr[props.index + i].imdbID;
+            i++;
+            popup(movie);
+          }
         });
       }
-    });
+    );
   };
   return (
     <a
-      onClick={popup}
+      onClick={() => popup(item.imdbID)}
       className="col-lg-6 border rounded mediaQ"
       style={{ minHeight: "200px" }}
     >
